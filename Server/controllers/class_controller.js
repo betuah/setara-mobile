7e50e5405662b8d5dc01978f8749b42bc0c7a0d9
Ihsan-Fauzi <ihsanfauzi.id@gmail.com>
2020-12-12 19:38:01 +0700

    Add posting detil function

--- Server/controllers/class_controller.js
+++ Server/controllers/class_controller.js
@@ -210,6 +210,75 @@ exports.joinClass = async (req, res) => {
     }
 }
 
+exports.getDetailPosting = async (req, res) => {
+    const id_posting  = req.params.postingId
+
+    const getDetails = new Promise(async (resolve, reject) => {
+        try {
+            const dataPosting = await Posting.findOne({ _id: id_posting })
+
+            if(dataPosting){
+                try {
+                    const creator = await User.findOne({_id: dataPosting.creator})
+                    const kelas   = await Kelas.findOne({_id: dataPosting.id_kelas})
+
+                    AnggotaKelas.findOne({id_user: dataPosting.creator, id_kelas: dataPosting.id_kelas}).then(anggotaKelas => {
+                        if(anggotaKelas){
+                            switch (anggotaKelas.status) {
+                                case '1':
+                                    status_creator = 'Administrator Kelas'
+                                    break;
+                                case '2':
+                                    status_creator = 'Tutor (Pengampu Mata Pelajaran)'
+                                    break;
+                                case '3':
+                                    status_creator = 'Tutor (Pendamping)'
+                                    break;
+                                default:
+                                    status_creator = 'Warga Belajar'
+                                    break;
+                            }
+                        }else{
+                            status_creator = 'Administrator Kelas'
+                        }
+                    }).finally(() => {
+                        const newDataPosting = {
+                            ...dataPosting._doc,
+                            foto_creator: `${env.picture_path}${creator.foto}`,
+                            nama_creator: creator.nama,
+                            status_creator: status_creator,
+                            nama_kelas: kelas.nama
+                        }
+                        resolve(newDataPosting)
+                    })
+                } catch (e) {
+                    console.log(new Error(e))
+                }
+            }else{
+                reject([])
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+    getDetails.then(resDetailKelas => {
+        const resJson = {
+            code: 'OK',
+            status: 'Success',
+            data: resDetailKelas
+        }
+
+        res.status(200).json(resJson)
+    }).catch(err => {
+        if (err.code) {
+            res.status(err.code).json(err)
+        } else {
+            res.status(500).json(err)
+        }
+    })
+}
+
 exports.getPosting = async (req, res) => {
     const id_kelas  = req.params.classId
 
