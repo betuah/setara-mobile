e135fa81b7555f778902cdb1272cd9f8edcc0891
Betuah Anugerah <betuahanugerah@gmail.com>
2020-12-18 15:20:03 +0700

    Remove fsRN from main apps android

--- SetaraDaring/android/app/src/main/java/com/setaradaring/MainApplication.java
+++ SetaraDaring/android/app/src/main/java/com/setaradaring/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.rnfs.RNFSPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
