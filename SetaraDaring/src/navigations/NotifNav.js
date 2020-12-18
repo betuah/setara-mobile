6beadbb139cad5b5863ac294f10305f91b2d8916
Betuah Anugerah <betuahanugerah@gmail.com>
2020-12-18 14:54:31 +0700

    Fix status bar notif

--- SetaraDaring/src/navigations/NotifNav.js
+++ SetaraDaring/src/navigations/NotifNav.js
@@ -14,6 +14,7 @@ import Header from '../components/common/Header';
 
 import * as authAct from '../store/actions/authAction';
 import * as notifAct from '../store/actions/notifActions';
+import { StatusBar } from 'react-native';
 
 const Stack = createStackNavigator();
 const Tab = createMaterialTopTabNavigator();
@@ -137,6 +138,8 @@ const NotifNav = (props) => {
     const ScreenOptionStyle = {
         headerShown: true,
         header: ({ scene, previous, navigation }) => (
+            <>
+            <StatusBar backgroundColor={colors.primary} barStyle='light-content' />
             <Header 
                 data={{
                     title: 'N O T I F I K A S I', 
@@ -154,6 +157,7 @@ const NotifNav = (props) => {
                     shadowRadius: 0,
                 }}
             />
+            </>
         ),
     }
 
