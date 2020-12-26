3842573490031ce781b624348b11154bdd8ad1cb
betuah <betuahanugerah@gmail.com>
2020-12-27 00:05:38 +0700

    Update Android setting like mainfest and gradle

--- SetaraDaring/android/app/src/main/java/com/setaradaring/MainApplication.java
+++ SetaraDaring/android/app/src/main/java/com/setaradaring/MainApplication.java
@@ -4,9 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import org.wonday.orientation.OrientationPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -66,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
     if (BuildConfig.DEBUG) {
       try {
         /*
-        We use reflection here to pick up the class that initializes Flipper,
+         We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
         Class<?> aClass = Class.forName("com.setaradaring.ReactNativeFlipper");
